9522ba3967366b6ca1ae1e60f0c9eed214c9171e
zzlhr <2388399752@qq.com>
2018-08-27 10:09:00 +0800

    demo03 完成

--- demo03.py
+++ demo03.py
@@ -14,13 +14,12 @@ with tf.Session() as sess:
     result = sess.run([mul, add])
     print(result)
 
-
 # Feed
 # 创建占位符
 input1 = tf.placeholder(tf.float32)
 input2 = tf.placeholder(tf.float32)
 output = tf.multiply(input1, input2)
 
-
 with tf.Session() as sess:
-    print(sess.run())
+    # feed的数据以字典的形式传入
+    print(sess.run(output, feed_dict={input1: [8.], input2: [2.]}))
